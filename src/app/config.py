c340247db8d97337416850f685ab489ba94978ff
Nolan Conaway <nolanbconaway@gmail.com>
2021-04-10 10:28:37 -0400

    bump to 0.5.1

--- src/app/config.py
+++ src/app/config.py
@@ -3,21 +3,7 @@ import sqlfluff
 
 SQLFLUFF_VERSION = sqlfluff.__version__
 
-# manually updated on version bump
-VALID_DIALECTS = (
-    "ansi",
-    "bigquery",
-    "mysql",
-    "teradata",
-    "postgres",
-    "snowflake",
-    "exasol",
-    "exasol_fs",
-)
-
+VALID_DIALECTS = tuple(d.name for d in sqlfluff.list_dialects())
 
 # dict mapping string rule names to descriptions
-VALID_RULES = {
-    r.code: r.description
-    for r in sqlfluff.core.rules.std_rule_set.get_rulelist(sqlfluff.core.FluffConfig())
-}
+VALID_RULES = {r.code: r.description for r in sqlfluff.list_rules()}
